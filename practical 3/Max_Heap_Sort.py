a35bac2bf34e14eb7fd969af86f8a036705ff5bd
BHAVANA DINESH <dinesh.bhavana131462@marwadiuniversity.ac.in>
2026-08-07 16:10:32 +0530

    Update Max_Heap_Sort.py

--- practical 3/Max_Heap_Sort.py
+++ practical 3/Max_Heap_Sort.py
@@ -1,1 +1,52 @@
 
+# Max-Heap Sort in Python
+
+def heapify(arr, n, i):
+    largest = i
+    left = 2 * i + 1
+    right = 2 * i + 2
+
+    # Check left child
+    if left < n and arr[left] > arr[largest]:
+        largest = left
+
+    # Check right child
+    if right < n and arr[right] > arr[largest]:
+        largest = right
+
+    # If largest is not the root
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+
+        # Recursively heapify the affected subtree
+        heapify(arr, n, largest)
+
+
+def heap_sort(arr):
+    n = len(arr)
+
+    # Build Max Heap
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+
+    # Extract elements one by one
+    for i in range(n - 1, 0, -1):
+        arr[0], arr[i] = arr[i], arr[0]
+
+        heapify(arr, i, 0)
+
+
+# User input
+n = int(input("Enter the number of elements: "))
+
+arr = []
+print("Enter the elements:")
+
+for i in range(n):
+    arr.append(int(input()))
+
+print("Original array:", arr)
+
+heap_sort(arr)
+
+print("Sorted array:", arr)
