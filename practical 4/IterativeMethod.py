8dc6bb1f1f7b873f50524ab5bee6e4d039d5ba6b
BHAVANA DINESH <dinesh.bhavana131462@marwadiuniversity.ac.in>
2026-08-21 15:56:28 +0530

    Update IterativeMethod.py

--- practical 4/IterativeMethod.py
+++ practical 4/IterativeMethod.py
@@ -9,15 +9,4 @@ for i in range(1, n + 1):
 
 print("Factorial of", n, "=", fact)
 
-# Factorial using Recursive Method
 
-def factorial(n):
-    if n == 0 or n == 1:
-        return 1
-    else:
-        return n * factorial(n - 1)
-
-
-n = int(input("Enter a number: "))
-
-print("Factorial of", n, "=", factorial(n))
