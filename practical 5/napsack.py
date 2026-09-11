5adba22951745179f4cd9f0271bf1affba62ea4b
BHAVANA DINESH <dinesh.bhavana131462@marwadiuniversity.ac.in>
2026-09-11 16:03:40 +0530

    Create napsack.py

---
+++ practical 5/napsack.py
@@ -0,0 +1,30 @@
+def knapsack(W, val, wt):
+    n = len(wt)
+
+    # Create DP table
+    dp = [[0 for _ in range(W + 1)] for _ in range(n + 1)]
+
+    for i in range(1, n + 1):
+        for w in range(1, W + 1):
+
+            if wt[i - 1] <= w:
+                # Include or exclude the item
+                dp[i][w] = max(
+                    val[i - 1] + dp[i - 1][w - wt[i - 1]],
+                    dp[i - 1][w]
+                )
+            else:
+                # Cannot include the item
+                dp[i][w] = dp[i - 1][w]
+
+    return dp[n][W]
+
+
+n = int(input("Enter number of items: "))
+
+wt = list(map(int, input("Enter weights: ").split()))
+val = list(map(int, input("Enter values: ").split()))
+
+W = int(input("Enter capacity: "))
+
+print("Maximum value =", knapsack(W, val, wt))
